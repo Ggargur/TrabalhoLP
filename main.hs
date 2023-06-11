dcf0ad0456d196c06b4892148ae76682c833cba0
Tavress <gargur2002@gmail.com>
2023-06-11 00:13:36 -0300

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,29 +1,54 @@
 import           Data.Text
 
---  Exemplo de programa : b1?;π;(1?;πs)*;1?
 -- Operadores ; | U | * | ?
 -- Uma expressão pode ser terminal, um teste, uma escolha ou um interador
-data Expression = Terminal Identifier
-                | Test Expression
-                | Choice Expression Expression
-                | Iteration Expression
-
--- Uma transição entre estados e a expressão que revela essa transição
-data Transition = Transition State State Expression
-
--- Um estado no grafo
-type State = String
+--  Exemplo de programa : b1?;π;(1?;πs)*;1?
+data Expression = Binary Operator Expression Expression
+                | Uni Operator Expression
+                | Nuclear Identifier
+  deriving (Show)
 
 -- Um identificador no programa
 type Identifier = String
+
+instance Show Operator where
+  show :: Operator -> String
+  show Sequencial = ";"
+  show Choice = "U"
+  show Iteration = "*"
+  show Test = "?"
+
+data Operator = Sequencial
+              | Choice
+              | Iteration
+              | Test
+
+-- Uma transição entre estados e a expressão que revela essa transição
+newtype Transition = Transitions [(State, State, Expression)]
+
+-- Um estado de uma transição
+type State = String
 
 -- Aqui a gente vai separar o programa em programas menores
 -- Quando achar os aspas chamanado a função recursivamente
 getProgram program = do
   print program
 
+getOperator :: Expression -> Maybe Operator
+getOperator (Binary op _ _) = Just op
+getOperator (Uni op _) = Just op
+getOperator (Nuclear _) = Nothing
+
 main :: IO ()
 main = do
+  -- A U B
+  let programA = Binary Choice (Nuclear "A") (Nuclear "B")
+  -- A U (A ; B)
+  let programB = Binary
+        Choice
+        (Nuclear "A")
+        (Binary Sequencial (Nuclear "A") (Nuclear "B"))
   putStrLn "State your program:"
+  let tree = Transitions [("A", "B", Nuclear "A"), ("B", "C", Nuclear "B")]
   program <- getLine
   getProgram program
